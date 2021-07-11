248fa0f7b227dbbdf8b0f491057c4de67bbb163f
grasotus <grasotus@googlemail.com>
2021-07-11 18:34:07 +0200

    changed project structure

--- src/text-recognizer.py
+++ src/text-recognizer.py
@@ -4,6 +4,6 @@ import pytesseract
 img = Image.open("img/all-star.jpg")
 result = pytesseract.image_to_string(img)
 
-with open("text_result.txt", mode="w") as file:
+with open("text/text_result.txt", mode="w") as file:
     file.write(result)
     print("done")
