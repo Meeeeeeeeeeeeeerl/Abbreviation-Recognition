6c52db9775f19b31a73c51d3c29f2d4070e896bf
grasotus <grasotus@googlemail.com>
2021-07-11 20:18:39 +0200

    added abbreviation recogniser

--- src/text_recognizer.py
+++ src/text_recognizer.py
@@ -1,8 +1,10 @@
 from PIL import Image
 import pytesseract
 from image_enhancer import enhancer
+from abbreviation_recognicer import getAbbreviations
 image_path = "img/example.png"
 image_string = enhancer(image_path)
+print(getAbbreviations(image_string))
 
 with open("text/text_result.txt", mode="w") as file:
     file.write(image_string)
