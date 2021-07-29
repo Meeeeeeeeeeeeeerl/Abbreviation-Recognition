ea04bb8512e136c2a1e3959c637f957ca363c625
grasotus <grasotus@googlemail.com>
2021-07-29 15:24:06 +0200

    comments for binarization added; outputs extra picture

--- src/PreProcessing.py
+++ src/PreProcessing.py
@@ -2,11 +2,15 @@ import cv2
 
 def enhance(image_path, edited_image_path):
 
-    # Open the image and save it to the variable
+    # Opens the image and save it to the variable
     img = cv2.imread(image_path)
-    img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img_edit = cv2.adaptiveThreshold(img_grey,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)   
 
+    # Changes the color of the image to a greyscale.
+    img_edit = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Applies Binarization using an adaptive threshold. It decides if a pixel is black or white using its position and neighbour pixels (Gaussian Adaptive Threshold)
+    img_edit = cv2.adaptiveThreshold(img_edit,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)   
+    cv2.imwrite("img/picture-binarized.png", img_edit)
     # Saves the edited image to the given path
     cv2.imwrite(edited_image_path, img_edit)
 
