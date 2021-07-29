f7f3a9eaae2998d63dd85f9ae16cd6bc8c402f87
grasotus <grasotus@googlemail.com>
2021-07-29 15:19:03 +0200

    changed pre-processing to cvs; added binarization

--- src/PreProcessing.py
+++ src/PreProcessing.py
@@ -1,19 +1,14 @@
-from PIL import Image, ImageEnhance
+import cv2
 
 def enhance(image_path, edited_image_path):
 
     # Open the image and save it to the variable
-    img = Image.open(image_path)
-
-    # Increases the sharpness and contrast of the image to help the text recognition
-    enhancer_sharp = ImageEnhance.Sharpness(img)
-    enhancer_contrast = ImageEnhance.Contrast(img)
-
-    img_edit = enhancer_sharp.enhance(25.0)
-    img_edit = enhancer_contrast.enhance(3.0)
+    img = cv2.imread(image_path)
+    img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_edit = cv2.adaptiveThreshold(img_grey,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)   
 
     # Saves the edited image to the given path
-    img_edit.save(edited_image_path)
+    cv2.imwrite(edited_image_path, img_edit)
 
     # Returns edited image
     return img_edit
